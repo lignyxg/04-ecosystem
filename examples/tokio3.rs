0639c9e3b582f68c35d611456a19aa07c4475c16
lignyxg <lignyxg@qq.com>
2024-06-21 19:59:48 +0800

    feature: use tokio::task::spawn_blocking instead thread::spawn

--- examples/tokio3.rs
+++ examples/tokio3.rs
@@ -8,7 +8,6 @@ use tokio::sync::mpsc::Receiver;
 async fn main() -> anyhow::Result<()> {
     let (tx, rx) = mpsc::channel(42);
     let handler = worker(rx);
-
     tokio::spawn(async move {
         let mut i = 0;
         loop {
@@ -19,15 +18,26 @@ async fn main() -> anyhow::Result<()> {
         #[allow(unreachable_code)]
         Ok::<(), anyhow::Error>(())
     });
-    handler.await.join().unwrap();
+
+    handler.await.unwrap();
+
     Ok(())
 }
 
-async fn worker(mut rx: Receiver<String>) -> thread::JoinHandle<()> {
-    thread::spawn(move || {
+async fn worker(mut rx: Receiver<String>) -> anyhow::Result<()> {
+    // thread::spawn(move || {
+    //     while let Some(s) = rx.blocking_recv() {
+    //         thread::sleep(Duration::from_millis(500));
+    //         println!("received: {}", s);
+    //     }
+    // })
+
+    tokio::task::spawn_blocking(move || {
         while let Some(s) = rx.blocking_recv() {
             thread::sleep(Duration::from_millis(500));
             println!("received: {}", s);
         }
     })
+    .await?;
+    Ok(())
 }
